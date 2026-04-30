81eaef981b32dd73ae55d83b26c9b9677e591eca
Brunosouzad <bs88994@gmail.com>
2026-04-30 15:14:30 -0300

    fix: adicionando 'Comercial' e 'Industrial' aos tipos permitidos de categoria

--- src/data/properties.ts
+++ src/data/properties.ts
@@ -15,7 +15,7 @@ export interface Property {
   garages: number;
   area: number;
   type: "Venda" | "Aluguel";
-  category: "Apartamento" | "Área" | "Barracão" | "Casa" | "Chácara" | "Fazenda" | "Galpão" | "Loja" | "Lote" | "Prédio" | "Sala" | "Sítio" | "Quitinete" | "Pousada";
+  category: "Apartamento" | "Área" | "Barracão" | "Casa" | "Chácara" | "Fazenda" | "Galpão" | "Loja" | "Lote" | "Prédio" | "Sala" | "Sítio" | "Quitinete" | "Pousada" | "Comercial" | "Industrial";
   image: string;
   images?: string[];
   features?: string[];

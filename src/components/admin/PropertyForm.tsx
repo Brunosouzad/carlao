1c42a7ba008306feb16786ba08b83630a7b9d36a
Brunosouzad <bs88994@gmail.com>
2026-04-30 15:10:46 -0300

    fix: adicionando import faltante do useEffect no PropertyForm

--- src/components/admin/PropertyForm.tsx
+++ src/components/admin/PropertyForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { Property } from "@/data/properties";
 import { useProperties } from "@/store/PropertiesContext";

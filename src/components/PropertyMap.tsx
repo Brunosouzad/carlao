c4e7cf67ac760cf30b16d399cf080bf2e8a713a3
Brunosouzad <bs88994@gmail.com>
2026-05-06 23:33:47 -0300

    fix: remove dynamic key from MapContainer to prevent leaflet crash

--- src/components/PropertyMap.tsx
+++ src/components/PropertyMap.tsx
@@ -134,7 +134,6 @@ export default function PropertyMap({ location, title, className = "h-[350px]", 
   return (
     <div className={`${className} rounded-none overflow-hidden border border-slate-200 shadow-sm relative z-0`}>
       <MapContainer
-        key={`${coords[0]}-${coords[1]}`}
         center={coords}
         zoom={15}
         scrollWheelZoom={false}
